30696f6d146c7efbc99c75ea608a3fae1d937627
Iago Cavalcante <iagoangelimc@gmail.com>
2019-10-13 22:33:20 -0300

    add: (WIP) create input components and scratch login page

---
+++ src/components/LInput.vue
@@ -0,0 +1,64 @@
+<template>
+  <input
+    class="legends-input"
+    :type="type"
+    :value="value"
+    :name="name"
+    @input="$emit('input', $event.target.value)"
+    :placeholder="placeholder"
+  >
+</template>
+
+<script>
+export default {
+  name: 'LInput',
+  props: {
+    value: {
+      required: false
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'text'
+    },
+    name: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    id: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    verifiyError: {
+      required: false
+    }
+  },
+  watch: {
+    verifiyError: function (val) {
+      if (val === true) {
+        this.className = 'text-input-error'
+        this.$emit('resetClass', this.className)
+      } else {
+        this.className = 'text-input'
+        this.$emit('resetClass', this.className)
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+.legends-input
+  color: rgb(190, 25, 101)
+
+.legends-input:focus
+  outline: none
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="legends">LEGENDS<b class="match">MATCH</b></h1>
+    <div class="form-input">
+      <LInput/>
+    </div>
+    <div class="form-input">
+      <LInput/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LInput from '@/components/LInput.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    LInput
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.legends
+  font-weight: lighter
+  color: #fff
+
+.match
+  font-weight: bold
+  color: #fff
+
+.home
+  text-align: center
+
+.form-input
+  margin-top: 10px
+</style>
